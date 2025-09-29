773d471ffad1ba29d19543d6f74f19445f743b35
karthiga2092-alt <karthiga2092@gmail.com>
2025-09-29 15:44:46 +0530

    changes

--- module3-solution/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#navbar-collapse').on('shown.bs.collapse', function () {
-      $('.navbar-toggle').addClass('toggled-open');
-    });
-    $('#navbar-collapse').on('hidden.bs.collapse', function () {
-      $('.navbar-toggle').removeClass('toggled-open');
-    });
-  });
